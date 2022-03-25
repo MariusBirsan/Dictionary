545cb7801b7a2607a8266854bfccb3741c36f3cf
Marius Birsan <100488307+MariusBirsan@users.noreply.github.com>
2022-03-25 21:01:55 +0200

    Add files via upload

--- Dictionary.js
+++ Dictionary.js
@@ -1,13 +1,22 @@
 var addedWords = [];
 function checkWords() {
+    let word = document.getElementById("checkWord").value;
+    if (addedWords.includes(word)) {
+        Message.innerHTML = "The word was already added. Please enter another word!";
+        document.getElementById("checkWord").value = "";
+    } else {
+        Message.innerHTML = "Word can be added in the dictionary!";
+        document.getElementById("checkWord").value = "";
+    }
+}
+function addWords() {
     let word = document.getElementById("addedWord").value;
     if (addedWords.includes(word)) {
-        searchWord.innerHTML = "The word was already added. Please enter another word.";
+        Message.innerHTML = "The word was already added. Please enter another word!";
         document.getElementById("addedWord").value = "";
     } else {
-        searchWord.innerHTML = "Word was succesfully added!";
+        Message.innerHTML = "Word was succesfully added!";
         addedWords.push(word);
         document.getElementById("addedWord").value = "";
     }
-    
 }
