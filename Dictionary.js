cfe9d1eff223e18c175c6dbc835057ba9b57ad54
Marius Birsan <100488307+MariusBirsan@users.noreply.github.com>
2022-03-26 16:05:25 +0200

    Update Dictionary.js

--- Dictionary.js
+++ Dictionary.js
@@ -1,21 +1,21 @@
-var addedWords = [];
+let addedWords = [];
 function checkWords() {
     let word = document.getElementById("checkWord").value;
     if (addedWords.includes(word)) {
-        Message.innerHTML = "The word was already added. Please enter another word!";
+        message.innerHTML = "The word was already added. Please enter another word!";
         document.getElementById("checkWord").value = "";
     } else {
-        Message.innerHTML = "Word can be added in the dictionary!";
+        message.innerHTML = "Word can be added in the dictionary!";
         document.getElementById("checkWord").value = "";
     }
 }
 function addWords() {
     let word = document.getElementById("addedWord").value;
     if (addedWords.includes(word)) {
-        Message.innerHTML = "The word was already added. Please enter another word!";
+        message.innerHTML = "The word was already added. Please enter another word!";
         document.getElementById("addedWord").value = "";
     } else {
-        Message.innerHTML = "Word was succesfully added!";
+        message.innerHTML = "Word was succesfully added!";
         addedWords.push(word);
         document.getElementById("addedWord").value = "";
     }
